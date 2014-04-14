ec928a9baaf2a2024f73aae340ffe8bb35ddaf17
Yang Ge <eroicaleo@gmail.com>
2014-04-14 00:38:59 -0700

    Added more code

--- LeetCode/BinarySearchTreeInOrderTraversalIterativeSolution/BST.cpp
+++ LeetCode/BinarySearchTreeInOrderTraversalIterativeSolution/BST.cpp
@@ -14,6 +14,23 @@ struct Node {
 
 typedef pair<Node*, NodeStatus> item;
 
+void BST2(Node *root) {
+    stack<Node *> s;
+    Node* current = root;
+    while(!s.empty() || current) {
+	if (current) {
+	    s.push(current);
+	    current = current->left;
+	} else {
+	    current = s.top();
+	    cout << "Node data is: " << current->data << endl;
+	    s.pop();
+	    current = current->right;
+	}
+    }
+    return;
+}
+
 void BST(Node *root) {
     if (!root) {
 	cout << "The tree is empty!" << endl;
@@ -24,18 +41,17 @@ void BST(Node *root) {
     s.push(make_pair(root, NotVisited));
 
     while (!s.empty()) {
-	item top = s.top();
+	item &top = s.top();
 	Node* n {top.first->left};
-	cout << top.first->data << endl;
 	if ((top.second == NotVisited) && n) {
-	    s.push(make_pair(top.first, NotVisited));
+	    s.push(make_pair(n, NotVisited));
 	    top.second = LeftVisited;
 	} else {
 	    cout << "Node data is: " << top.first->data << endl;
 	    n = top.first->right;
 	    s.pop();
 	    if (n) {
-		s.emplace(n, NotVisited);
+		s.push(make_pair(n, NotVisited));
 	    }
 	}
     }
@@ -52,5 +68,6 @@ int main() {
 	    vNode[i].right = &vNode[2*i+2];
 	}
     }
-    BST(&vNode[0]);
+    // BST(&vNode[0]);
+    BST2(&vNode[0]);
 }
